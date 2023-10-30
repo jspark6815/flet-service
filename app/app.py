48bd1c9bfd853be1de5a344fdc61e7b35ff813a7
jspark6815 <jspark6815@daum.net>
2023-10-31 01:58:14 +0900

    feat: add page text

--- app/app.py
+++ app/app.py
@@ -2,5 +2,6 @@ import flet as ft
 print("Flet Web Server is starting...")
 def main(page: ft.Page):
     page.add(ft.Text(value="Containerized Flet app!"))
+    page.add(ft.Text(value="feat. js.park"))
 
 ft.app(target=main, view=None, port=8080)
